7c08e0ef5e06655abd82188e485514437c91774b
mugiwaraace <filibertoerios@gmail.com>
2021-04-03 12:22:31 -0500

    made methods to set local/remote paths dynamically instead of at object creation; added recursive up/download capability

--- client.py
+++ client.py
@@ -4,12 +4,13 @@ from scp import SCPClient
 
 class Client:
 
-    def __init__(self, host: str, username: str, password: str, local_path: str, remote_path: str):
+    def __init__(self, host: str, username: str, password: str):
         self.__host = host
         self.__username = username
         self.__password = password
-        self.__local_path = local_path
-        self.__remote_path = remote_path
+        self.__local_path = None
+        self.__remote_path = None
+        self.__recursive = False
         self.__set_client()
 
     def __set_client(self):
@@ -19,16 +20,25 @@ class Client:
         client.connect(self.__host, username=self.__username, password=self.__password)
         self.__client = client
 
+    def set_local_path(self, local_path: str):
+        self.__local_path = local_path
+
+    def set_remote_path(self, remote_path: str):
+        self.__remote_path = remote_path
+
+    def set_recursive(self, recursive: bool):
+        self.__recursive = recursive
+
     def upload(self):
         self.__client.connect(self.__host, username=self.__username, password=self.__password)
         with SCPClient(self.__client.get_transport()) as scp:
-            scp.put(self.__local_path, self.__remote_path)
+            scp.put(self.__local_path, self.__remote_path, recursive=self.__recursive)
         self.__client.close()
 
     def download(self):
         self.__client.connect(self.__host, username=self.__username, password=self.__password)
         with SCPClient(self.__client.get_transport()) as scp:
-            scp.get(self.__remote_path, self.__local_path)
+            scp.get(self.__remote_path, self.__local_path, recursive=self.__recursive)
         self.__client.close()
 
     def close_client(self):
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scp import SCPException
 from getpass import getpass
 
 USAGE = "script.py [--upload | --download] [--username <username>] <host> " \
-        "[<localpath> | <remotepath>] [<remotepath> | <localpath>]"
+        "[<localpath> | <remotepath>] [<remotepath> | <localpath>] [--recursive | -r]"
 
 
 def main():
@@ -17,6 +17,9 @@ def main():
     up_down = ''
     local_path = ''
     remote_path = ''
+    recurse = False
+    if len(sys.argv[1:]) == 7 and (sys.argv[7] == '--recursive' or sys.argv[7] == '-r'):
+        recurse = True
     if sys.argv[1] == '--upload':
         up_down = 'up'
         local_path = os.path.abspath(sys.argv[5])
@@ -38,7 +41,10 @@ def main():
     username = sys.argv[3]
     password = getpass("Password: ")
 
-    client = Client(host, username, password, local_path, remote_path)
+    client = Client(host, username, password)
+    client.set_local_path(local_path)
+    client.set_remote_path(remote_path)
+    client.set_recursive(recurse)
 
     try:
         if up_down == 'up':
@@ -48,11 +54,11 @@ def main():
             client.download()
             print(f"Success in downloading '{remote_path}' from '{host}' to '{local_path}'")
     except TimeoutError:
-        print(f"Couldn't connect to {host}")
+        print(f"Failed to connect to {host}")
         print("Verify destination and try again.")
         sys.exit(1)
     except SCPException:
-        print("Couldn't process file")
+        print("Failed to process file")
         print("Verify file and try again")
         sys.exit(1)
     finally:
